3d35592b6dc692cf7e654e87980052572fc3bd84
Erik Zakrzewski and Kelsey Langlois <erik+kelsey@>
2017-11-28 14:07:16 -0800

    Display edit form in answers feed.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,6 +11,18 @@ class AnswersController < ApplicationController
       redirect_to question_path(@question)
     end
   end
+
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if current_user == @answer.user
+      render template: "questions/show"
+    else
+      flash[:alert] = "You aren't authorized to do that."
+      redirect_to question_path(@question)
+    end
+  end
+
   private
     def answer_params
       params.require(:answer).permit(:content)
